488245b3779e9ac9faf863096b983159a8b2bc62
Juan David <juanda7473@gmail.com>
2021-08-17 13:17:03 -0500

    update ScrumBoard

--- backend/controllers/role.js
+++ backend/controllers/role.js
@@ -1,28 +1,28 @@
 const Role = require("../models/role");
 
 const registerRole = async (req, res) => {
+  //valido que no me lleguen valores null
   if (!req.body.name || !req.body.description)
     return res.status(401).send("Process failed: Incomplete data");
 
   const existingRole = await Role.findOne({ name: req.body.name });
   if (existingRole)
     return res.status(401).send("Process failed: role already exist");
+
   const role = new Role({
     name: req.body.name,
     description: req.body.description,
     dbStatus: true,
   });
-
   const result = await role.save();
   if (!result) return res.status(401).send("Failed to register role");
   return res.status(200).send({ role });
 };
-
+//lista los roles
 const listRole = async (req, res) => {
-    const role=await Role.find()
-    if(!role || user.length === 0) return res.status(401).send("No Role")
-    return res.status(200).send({role})
+  const role = await Role.find();
+  if (!role || role.length === 0) return res.status(401).send("No Role");
+  return res.status(200).send({ role });
 };
 
 module.exports = { registerRole, listRole };
- 
--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,17 @@
 const express = require("express");
+
 const cors = require("cors");
+
 const { dbConnection } = require("./db/db");
 
 const Role = require("./routes/role");
 const User = require("./routes/user");
-const Board = require("./routes/board");
 const Auth = require("./routes/auth");
-
-
+const Board = require("./routes/board");
 require("dotenv").config();
 
 const app = express();
+
 app.use(express.json());
 
 app.use(cors());
@@ -23,4 +24,5 @@ app.use("/api/board/", Board);
 app.listen(process.env.PORT, () =>
   console.log("Backend server running OK, on port: ", process.env.PORT)
 );
+
 dbConnection();
