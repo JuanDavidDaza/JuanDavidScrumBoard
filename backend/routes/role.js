9615773ea477824e8cedc53c746e3495ce640af1
Juan David <juanda7473@gmail.com>
2021-08-23 15:31:52 -0500

    update ScrumBoard

--- backend/routes/role.js
+++ backend/routes/role.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const RoleController = require("../controllers/role");
+const Auth = require("../middleware/auth");
+const ValidateUser = require("../middleware/validateUser");
+const Admin = require("../middleware/admin");
 
-//GET POST PUT DELETE
-// http://localhost:3001/api/role/registerRole
-router.post("/registerRole", RoleController.registerRole);
-// http://localhost:3001/api/role/listRole
-router.get("/listRole", RoleController.listRole);
+router.post("/registerRole",  Auth,  ValidateUser,  Admin,  RoleController.registerRole);
+router.get("/listRole", Auth, ValidateUser, Admin, RoleController.listRole);
+router.put("/updateRole", Auth, ValidateUser, Admin, RoleController.updateRole);
 
-module.exports=router;
+module.exports = router;
